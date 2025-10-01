f2870afe0d2811649b98124761022836751afdb7
l.hellawell@outlook.com <l.hellawell@outlook.com>
2025-10-01 08:52:53 +0000

    with int orders operational working

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,2 +1,6 @@
+--- margin = revenue - purchase_cost
+--- purchase_cost = quantity * purchase_price
+--- Operational_margin = margin + shipping_fee - log_cost - ship_cost
+
 select *
-from {{ref('int_orders_margin') }} AS margin
+from {{ref('int_orders_margin') }}
