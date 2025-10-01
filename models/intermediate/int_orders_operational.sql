60979d22dfac76c077d68ad8c53a869751810038
l.hellawell@outlook.com <l.hellawell@outlook.com>
2025-10-01 09:05:19 +0000

    join completed and built

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,7 +2,11 @@
 --- purchase_cost = quantity * purchase_price
 --- Operational_margin = margin + shipping_fee - log_cost - ship_cost
 
-select *
+select
+    o.orders_id,
+    o.date_date,
+    SUM(margin + shipping_fee - logcost - ship_cost) as operational_margin
 from {{ref('int_orders_margin') }} as o
 left join {{ref('stg_raw_ship')}} as s
 on o.orders_id = s.orders_id
+group by o.orders_id, o.date_date
