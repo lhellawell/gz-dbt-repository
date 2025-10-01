6dac8f7b0b7a5e52bd907e80cbb2727651bb1225
l.hellawell@outlook.com <l.hellawell@outlook.com>
2025-10-01 13:08:49 +0000

    all cleaned

--- models/staging/raw/stg_raw__bing.sql
+++ models/staging/raw/stg_raw__bing.sql
@@ -12,10 +12,10 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        campgn_name,
-        ads_cost,
-        impression,
-        click
+        campgn_name as campaign_name,
+        cast(ads_cost as float64) as ads_cost,
+        cast(impression as integer) as impression,
+        cast(click as integer) as click
 
     from source
 
--- models/staging/raw/stg_raw__criteo.sql
+++ models/staging/raw/stg_raw__criteo.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('raw', 'facebook') }}
+    select * from {{ source('raw', 'criteo') }}
 
 ),
 
